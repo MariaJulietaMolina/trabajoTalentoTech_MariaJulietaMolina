be74250e4230bb775c7d6c54744b618638d2dd47
MariaJulietaMolina <mariajulietamolina91@gmail.com>
2025-05-17 18:33:28 -0300

    primer commi clase 3

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+import React from 'react'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Main from './components/Main'
+import Gallery from './components/Gallery'
+import Footer from './components/Footer'
+
+function App() { 
+  return ( 
+  <div> 
+  <Header /> 
+  <Nav /> 
+  <Main /> 
+  <Gallery /> 
+  <Footer />
+  </div> 
+  ); 
+ } 
+ export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f1f1f1",
+        padding: "10px",
+        textAlign: "center",
+        marginTop: "20px",
+      }}
+    >
+      <p>&copy; Derechos reservados @ MJM </p>
+    </footer>
+  );
+}
+
+export default footer;
